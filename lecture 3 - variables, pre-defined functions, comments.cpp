697617826545e71c2720f9a62df55df721f381aa
Aksam <aksam.iftikhar@gmail.com>
2025-02-19 11:13:54 +0500

    Everything until Lecture 3

---
+++ lecture 3 - variables, pre-defined functions, comments.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	// double length = 3.0, width = 4.5;
+	double length, width; // this is the length and width of a rectangle
+
+	/* following variables represent the area and perimeter of a rectangle,
+	whose length and width are given above. */
+	double area = length * width;
+	double perimeter;
+	perimeter = 2 * (length + width);
+
+	cout << length;
+
+	double area_sqrt = sqrt(area);
+	cout << pow(3, 4) << 3 * 3 * 3 * 3;
+
+	cout << "Hello, world!";
+	cout << endl;
+	cout << "Bye, world!";
+
+	cout << "Hello, world!" << endl
+		 << "Bye, world!";
+
+	return 1;
+}
