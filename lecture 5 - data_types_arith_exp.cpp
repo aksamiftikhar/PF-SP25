4e1a08edb078945b5cfa55270feaa3d8e2cebabc
maazmohsin2401 <maaz@immersive-fox.com>
2025-02-26 10:47:28 +0500

    Week 3 Completed

---
+++ lecture 5 - data_types_arith_exp.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	bool x = false;
+	long long x2 = 1233545345;
+	char c1 = 65;
+	int A = 20;
+	int rem = 25 % 6;
+	int result = 25 % 6 * (3 + 7) / 2;
+	cout << x << "  " << x2 << endl;
+	cout << c1 << " " << A << 'A';
+	cout << endl
+		 << rem << endl;
+
+	float a = 5 / 2;
+	float f = 2;
+	cout << 5 / 2 << endl;
+	cout << a;
+
+	return 0;
+}
